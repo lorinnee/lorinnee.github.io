4396de9d51cd7c3ad5346b347605b7216b68c5d2
lorinnee <lorenapurgante@gmail.com>
2022-06-17 18:21:46 +0200

    start + scheda 1

---
+++ schede/java_schede.js
@@ -0,0 +1,7 @@
+
+
+function changevid(buttonlink){
+document.getElementById('change').src = buttonlink;
+document.getElementById("button_right").disabled=true;
+document.getElementById("button_left").disabled=true;
+}
